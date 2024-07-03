8c12a6cde4b9d3b4184f049a523918d54f51d881
leechpk <167122836+leechpk@users.noreply.github.com>
2024-07-03 06:49:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/leechpk/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/leechpk/RoxyPostbot.git /RoxyPostbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /RoxyPostbot
 fi
 cd /TheMovieProviderBot
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting RoxyPostbot...."
 python3 bot.py
